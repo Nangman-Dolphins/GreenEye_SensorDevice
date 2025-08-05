22d30863db263f7c7fd1acb25ba86df88943c967
Jihoon Kim (Hanul) <jihoonkimtech@naver.com>
2025-08-05 21:12:31 +0900

    Update real-time value converter

--- Hardware/GreenEye Sensor Information/Soil EC Sensor/coefficient calculator.py
+++ Hardware/GreenEye Sensor Information/Soil EC Sensor/coefficient calculator.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-print("--- 3차 계수 산출기 ---")
+print("\n======= 3차 계수 산출기 ========\n")
 
 # Data points.
-x_coords = np.array([1920, 1987, 3128, 1948])
-y_coords = np.array([84, 1413, 12880, 233])
+x_coords = np.array([1920, 1987, 1948, 3128])
+y_coords = np.array([84, 1413, 233, 12880])
 
 coefficients = np.polyfit(x_coords, y_coords, 3)
 
@@ -25,3 +25,24 @@ print("--- 로컬 계수 검증 결과 ---")
 for x, y_expected in zip(x_coords, y_coords):
     y_calculated = convert_sensor_value_from_local_coeffs(x, coefficients)
     print(f"입력: {x}, 기대값: {y_expected}, 계산값: {y_calculated:.2f}")
+
+print("\n" + "="*50)
+print("   실시간 값 변환기 (input 'q' for quit)")
+print("="*50)
+
+while True:
+    user_input = input("ADC_raw: ")
+
+    if user_input.lower() == 'q':
+        print("Bye\n")
+        break
+    
+    try:
+        input_value = float(user_input)
+
+        result = convert_sensor_value_from_local_coeffs(input_value, coefficients)
+
+        print(f"-> result: {result:.2f}\n")
+
+    except ValueError:
+        print("ERR: Wrong Input.\n")
