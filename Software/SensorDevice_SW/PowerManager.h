f53fda53e0a866af1ed8f6f5add64e0c244dcdfc
Jihoon Kim (Hanul) <jihoonkimtech@naver.com>
2025-08-29 22:13:16 +0900

    Update debug mode, get mode string

--- Software/SensorDevice_SW/PowerManager.h
+++ Software/SensorDevice_SW/PowerManager.h
@@ -1,3 +1,5 @@
+// PowerManager.h
+
 #pragma once // prevents multiple inclusion of the header file
 
 #include <Preferences.h> // for non-volatile storage
@@ -7,7 +9,8 @@ enum PowerMode { // defines the available power modes
     LOW_POWER,       // l
     NORMAL,          // m
     HIGH_FREQ,       // h
-    ULTRA_HIGH_FREQ  // u
+    ULTRA_HIGH_FREQ, // u
+    DEBUGGING        // d // ADDED: Debugging mode enum
 };
 
 class PowerManager {
@@ -26,6 +29,8 @@ private:
             case NORMAL:          _senseInterval = 30 * 60;  _camInterval = 1 * 3600; break;
             case HIGH_FREQ:       _senseInterval = 20 * 60;  _camInterval = 1 * 3600; break;
             case ULTRA_HIGH_FREQ: _senseInterval = 20 * 60;  _camInterval = 40 * 60;  break;
+            // ADDED: Interval settings for the new debugging mode
+            case DEBUGGING:       _senseInterval = 30;       _camInterval = 60;       break;
         }
         if (_debug_enabled) { // check if debug mode is on
             Serial.printf("[PM] Mode updated. Sense interval: %lu s, Cam interval: %lu s\n", _senseInterval, _camInterval);
@@ -51,7 +56,7 @@ public:
 
     void setMode(PowerMode newMode) { // sets a new power mode
         _currentMode = newMode; // update the internal mode
-        if (_debug_enabled) { Serial.print("[PM] Set Mode to "); Serial.println(_currentMode); }
+        if (_debug_enabled) { Serial.print("[PM] Set Mode to "); Serial.println(getModeString()); }
         updateIntervals(); // update the timing intervals
     }
 
@@ -62,6 +67,7 @@ public:
         else if (modeChar == 'M') newMode = NORMAL;
         else if (modeChar == 'H') newMode = HIGH_FREQ;
         else if (modeChar == 'U') newMode = ULTRA_HIGH_FREQ;
+        else if (modeChar == 'D') newMode = DEBUGGING; // ADDED: Set mode for 'D'
         
         if (newMode != _currentMode) { // if the mode actually changed
             setMode(newMode); // call the main setMode function
@@ -74,6 +80,19 @@ public:
         _nightModeEnabled = enabled; // update the internal flag
         _preferences.putBool("nht_mode", enabled); // save the setting to storage
         if (_debug_enabled) { Serial.printf("[PM] Night mode set to: %s and saved.\n", enabled ? "ON" : "OFF"); }
+    }
+
+    // ADDED: Function to get the current power mode as a string for display
+    String getModeString() {
+        switch (_currentMode) {
+            case ULTRA_LOW_POWER: return "초저전력";
+            case LOW_POWER:       return "저전력";
+            case NORMAL:          return "일반";
+            case HIGH_FREQ:       return "고빈도";
+            case ULTRA_HIGH_FREQ: return "초고빈도";
+            case DEBUGGING:       return "디버그 모드";
+            default:              return "Unknown";
+        }
     }
 
     unsigned long getSenseInterval() { return _senseInterval; } // returns the current sensor interval
